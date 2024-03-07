3e8a6eb62d86571d715fef0e2728c5dc657b4bbb
Correba <mat.correba@gmail.com>
2024-03-07 17:34:48 +0100

    Add routing in frontend app

--- my-app/webapp/src/app/app.module.ts
+++ my-app/webapp/src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule, Routes } from '@angular/router'
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ProducersComponent } from './components/producers/producers.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+const routes: Routes = [
+  { path: '', component: ProducersComponent },
+  { path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ProducersComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     HttpClientModule
   ],
   providers: [],
