da86da3c710641e73c0d462f670d088f5735bc18
Nini1551 <47613768+Nini1551@users.noreply.github.com>
2024-03-19 17:12:29 +0100

    update localeat/init-db.sql
    Replace transitory database structure for the registration management by
    another more complete which is made up the management of the user
    accounts, producer accounts and company accounts

--- local_eat/init-db.sql
+++ local_eat/init-db.sql
@@ -1,25 +1,45 @@
-CREATE TABLE producers (
-  id      INT AUTO_INCREMENT PRIMARY KEY,
-  name    VARCHAR(60) NOT NULL,
-  picture VARCHAR(60),
-  created DATE
-);
-
-CREATE TABLE users (
-  username VARCHAR(60) PRIMARY KEY,
-  password VARCHAR(255) NOT NULL,
-  email    VARCHAR(60) NOT NULL,
-  age      INT,
-  gender   VARCHAR(1),
-  address VARCHAR(60),
-  locality VARCHAR(60),
-  cellphone VARCHAR(60),
-  CHECK (gender IN ('M', 'F'))
-);
-
-INSERT INTO producers(name, picture, created) VALUES 
-('Alain', 'Alain.jpg', NOW()),
-('Jacques', 'Jacques.jpg', NOW()),
-('Marc', 'Marc.jpg', NOW()),
-('Mirco', 'Mirco.jpg', NOW()),
-('Tom', 'Tom.jpg', NOW());
+create table USER
+(USERNAME varchar(20) not null,
+ PASSWORD varchar(64) not null, -- SHA-256 produit un hash de 64 caractères hexadécimaux
+ MAIL varchar(50) not null,
+ primary key (USERNAME));
+ 
+ create table ADDRESS
+(ID char(5) not null,
+ STREET varchar(30) not null,
+ STREET_NUMBER int not null,
+ BOX_NUMBER varchar(5) null,
+ POSTCODE int not null,
+ LOCALITY varchar(20) not null,
+ COUNTRY varchar(20) not null,
+ primary key (ID));
+ 
+create table PRODUCER
+(ID char(7) not null,
+ USERNAME varchar(20) not null,
+ FIRSTNAME char(20) not null,
+ LASTNAME char(20) not null,
+ ADDRESS char(5) not null,
+ PHONE_NUM char(10) not null,
+ primary key (ID),
+ foreign key (USERNAME) references USER(USERNAME),
+ foreign key (ADDRESS) references ADDRESS(ID));
+ 
+create table COMPANY
+(COMPANY_NAME varchar(20) not null,
+ PASSWORD varchar(64) not null, -- SHA-256 produit un hash de 64 caractères hexadécimaux
+ ALIAS varchar(50) not null,
+ ADDRESS char(5) not null,
+ MAIL varchar(50) not null,
+ PHONE_NUM char(10) not null,
+ VAT_NUM char(12) not null,
+ DESCRIPTION longtext null,
+ primary key (COMPANY_NAME),
+ foreign key (ADDRESS) references ADDRESS(ID));
+  
+create table REL_COMP_PROD
+(PRODUCER_ID char(7) not null,
+ COMPANY_NAME varchar(20) not null,
+ primary key (PRODUCER_ID, COMPANY_NAME),
+ foreign key (PRODUCER_ID) references PRODUCER(ID),
+ foreign key (COMPANY_NAME) references COMPANY(COMPANY_NAME));
